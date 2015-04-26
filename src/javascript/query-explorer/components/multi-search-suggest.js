72a570a647dcaa5545136f6a44aa470e5779963f
Philip Walton <philip@philipwalton.com>
2015-04-26 14:22:18 -0700

    Separated the SearchSuggest component into a single and multi select.

---
+++ src/javascript/query-explorer/components/multi-search-suggest.js
@@ -0,0 +1,89 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/* global $ */
+
+
+import filter from 'lodash/collection/filter';
+import SearchSuggest from './search-suggest';
+
+
+export default class MultiSearchSuggest extends SearchSuggest {
+
+  handleChange(e) {
+    let value = e.target.value;
+    let prevSearch = this.parseValue(this.state.value).search;
+    let {search, selected} = this.parseValue(value);
+
+    // If the new value includes the old value, we only need to search
+    // through the existing matches rather than the full options set.
+    let options = search.includes(prevSearch) ?
+        this.state.matches : this.props.options;
+    let matches = this.findMatches(search, options, new Set(selected));
+
+    this.setState({value, matches, selectedMatchIndex: 0, open: true});
+    this.props.onChange.call(this, e);
+  }
+
+  setShowMatchesState() {
+    let {search} = this.parseValue(this.state.value);
+
+    // If search currently matches a known option and there are matches to
+    // choose from, add the separator and assume the user is trying to select
+    // another option.
+    if (this.optionsMap[search] && this.state.matches.length) {
+      let value = this.state.value + this.props.separator;
+
+      // Since we're updating the value, call `handleChange`.
+      this.handleChange({target:{value, name: this.props.name}});
+    }
+    super.setShowMatchesState();
+  }
+
+  setHideMatchesState() {
+    if (this.state.value.endsWith(this.props.separator)) {
+      let value = this.state.value.slice(0, -this.props.separator.length);
+      this.setState({value});
+      this.props.onChange.call(this, {target:{value, name: this.props.name}});
+    }
+    super.setHideMatchesState();
+  }
+
+  setSelectedMatchState(choice) {
+    let {search, selected} = this.parseValue(this.state.value);
+    let value = selected.concat([choice]).join(this.props.separator);
+    super.setSelectedMatchState(value);
+  }
+
+  findMatches(search, options = this.props.options, selected = new Set()) {
+    return filter(options, (option) =>
+        this.doesNotMatchSelected(option, selected) &&
+        this.matchesOption(search, option));
+  }
+
+  doesNotMatchSelected(option, selected) {
+    return !selected.has(option.id);
+  }
+
+  parseValue(value) {
+    let selected = value.split(this.props.separator);
+    let search = selected.pop();
+    return {search, selected};
+  }
+
+}
+
+MultiSearchSuggest.defaultProps = SearchSuggest.defaultProps;
+MultiSearchSuggest.defaultProps.separator = ',';
--- src/javascript/query-explorer/components/search-suggest.js
+++ src/javascript/query-explorer/components/search-suggest.js
@@ -30,13 +30,6 @@ const keyCodes = {
   ESC: 27,
 }
 
-
-// TODO(philipwalton):
-// - Only allow 1 segment.
-// - Add SearchSuggest to the "sort" option.
-// - handle the "no results found" case with input border radius weirdness.
-// - Remove legacy "XX" stuff.
-
 export default class SearchSuggest extends React.Component {
 
   constructor(props) {
@@ -45,13 +38,14 @@ export default class SearchSuggest extends React.Component {
       value: props.value,
       matches: this.findMatches(props.value),
       selectedMatchIndex: 0,
-      showMatches: false
+      open: false
     };
+    this.buildOptionsMap(this.props.options);
   }
 
   componentDidMount() {
     $(document).on(`click.SearchSuggest:${this.props.name}`, (e) => {
-      if (this.state.showMatches == true &&
+      if (this.state.open == true &&
           !$.contains(React.findDOMNode(this), e.target)) {
         this.setHideMatchesState();
       }
@@ -63,53 +57,73 @@ export default class SearchSuggest extends React.Component {
   }
 
   componentWillReceiveProps(props) {
+    if (props.options != this.props.options) {
+      this.buildOptionsMap(props.options);
+      this.setState({
+        matches: this.findMatches(this.state.value, props.options)
+      });
+    }
     if (props.value != this.state.value) {
       this.setState({value: props.value});
     }
   }
 
-  componentDidUpdate() {
-    let matches = this.state.matches;
-    let selectedMatchIndex = this.state.selectedMatchIndex;
-    let selectedMatch = matches[selectedMatchIndex];
+  componentDidUpdate(prevProps, prevState) {
 
-    // If there is no selected match, return immediately.
-    if (!(selectedMatch && this.refs[selectedMatch.id])) return;
-
-    let container = this.refs.matches.getDOMNode();
-    let containerScrollTop = container.scrollTop;
-    let containerScrollHeight = container.scrollHeight;
-    let containerClientHeight = container.clientHeight;
-
-    let selectedMatchElement = this.refs[selectedMatch.id].getDOMNode();
-    let selectedMatchElementOffsetTop = selectedMatchElement.offsetTop;
-    let selectedMatchElementClientHeight = selectedMatchElement.clientHeight;
-
-    // If the select match element is below the visible part of the container.
-    if (selectedMatchElementOffsetTop + selectedMatchElementClientHeight >
-        containerScrollTop + containerClientHeight) {
-
-      container.scrollTop = selectedMatchElementOffsetTop +
-          selectedMatchElementClientHeight - containerClientHeight;
+    // If the update was triggered by a value change, make sure the dropdown
+    // is always scrolled to the top so the first match is visible.
+    if (this.state.value != prevState.value) {
+      React.findDOMNode(this.refs.matches).scrollTop = 0;
     }
-    // If the select match element is above the visible part of the container.
-    else if (selectedMatchElementOffsetTop < containerScrollTop) {
-      container.scrollTop = selectedMatchElementOffsetTop;
+
+    // If the user is scrolling through the matches via the up and down arrows
+    // keys, make sure the selected match is always in view.
+    if (this.isKeyboardScrolling_) {
+
+      // TODO(philipwalton): is there a more React-y way of getting access
+      // to this node? Using refs seems like overkill...
+      let selectedElement = React.findDOMNode(this.refs.matches)
+          .children[this.state.selectedMatchIndex];
+
+      // If there is no selected match, return immediately.
+      if (!selectedElement) return;
+
+      let selectedElementOffsetTop = selectedElement.offsetTop;
+      let selectedElementClientHeight = selectedElement.clientHeight;
+      let container = React.findDOMNode(this.refs.matches);
+      let containerScrollTop = container.scrollTop;
+      let containerClientHeight = container.clientHeight;
+
+      // If the select match element is below the visible part of the container.
+      if (selectedElementOffsetTop + selectedElementClientHeight >
+          containerScrollTop + containerClientHeight) {
+
+        container.scrollTop = selectedElementOffsetTop +
+            selectedElementClientHeight - containerClientHeight;
+      }
+      // If the select match element is above the visible part of the container.
+      else if (selectedElementOffsetTop < containerScrollTop) {
+        container.scrollTop = selectedElementOffsetTop;
+      }
+
+      this.isKeyboardScrolling_ = false;
     }
   }
 
   handleChange(e) {
-    let newState = {
-      value: e.target.value,
-      matches: this.findMatches(e.target.value),
-      showMatches: true
-    }
+    let value = e.target.value;
 
-    if (this.state.selectedMatchIndex > newState.matches.length - 1) {
-      newState.selectedMatchIndex = 0;
-    }
+    // If the new value contains the old value, we only need to search
+    // through the existing matches rather than the full options set.
+    let matches = this.findMatches(value, value.includes(this.state.value) ?
+        this.state.matches : this.props.options);
 
-    this.setState(newState);
+    this.setState({
+      value,
+      matches,
+      selectedMatchIndex: 0,
+      open: true
+    });
     this.props.onChange.call(this, e);
   }
 
@@ -120,22 +134,24 @@ export default class SearchSuggest extends React.Component {
 
     switch (e.keyCode) {
       case keyCodes.DOWN_ARROW:
-        if (!this.state.showMatches) {
+        if (!this.state.open) {
           this.setShowMatchesState();
         }
         else if (selectedMatchIndex < totalMatches - 1) {
-          this.setState({selectedMatchIndex:  selectedMatchIndex + 1});
+          this.isKeyboardScrolling_ = true;
+          this.setState({selectedMatchIndex: selectedMatchIndex + 1});
         }
         e.preventDefault();
         break;
       case keyCodes.UP_ARROW:
         if (selectedMatchIndex > 0) {
+          this.isKeyboardScrolling_ = true;
           this.setState({selectedMatchIndex: selectedMatchIndex - 1});
         }
         e.preventDefault();
         break;
       case keyCodes.ENTER:
-        if (this.state.showMatches) {
+        if (this.state.open && selectedMatch) {
           this.setSelectedMatchState(selectedMatch.id);
         }
         else {
@@ -151,91 +167,61 @@ export default class SearchSuggest extends React.Component {
   }
 
   handleMouseEnter(index) {
-    this.setState({selectedMatchIndex: index});
+    if (!this.isScrolling_) this.setState({selectedMatchIndex: index});
+  }
+
+  handleScroll(e) {
+    this.isScrolling_ = true;
+    clearTimeout(this.scrollTimeout_);
+    this.scrollTimeout_ = setTimeout(() => this.isScrolling_ = false, 100);
   }
 
   setShowMatchesState() {
-    let {value, search} = this.parseValue(this.state.value);
-
-    // If search currently matches a known option, add a comma and assume the
-    // user is trying to select another option.
-    if (find(this.props.options, (option) => matches(option.id, search))) {
-      value = value + ',';
-    }
-
-    this.setState({
-      value: value,
-      matches: this.findMatches(value),
-      showMatches: true
-    });
+    this.setState({open: true, selectedMatchIndex: 0});
   }
 
   setHideMatchesState() {
-    // Remove a trailing comma or whitespace.
-    let value = this.state.value.replace(/[,\s]+$/, '');
+    this.setState({open: false});
+  }
 
-    this.setState({
-      value: value,
-      showMatches: false,
-      selectedMatchIndex: 0
-    });
-
+  setSelectedMatchState(value) {
+    this.setState({value, selectedMatchIndex: 0, open: false});
     this.props.onChange.call(this, {target:{value, name: this.props.name}});
   }
 
-  setSelectedMatchState(choice) {
-    let {parts, search} = this.parseValue(this.state.value);
-
-    // If the entered value (search) matches the choice, just keep it and
-    // assume that's what the user wanted.
-    if (matches(choice, search)) choice = search;
-
-    // Set the last part to the ID of the chosen option, overwriting whatever
-    // is currently there.
-    parts[parts.length - 1] = choice;
-
-    let value = parts.join(',');
-
-    this.setState({
-      value: value,
-      selectedMatchIndex: 0,
-      showMatches: false
-    });
-
-    this.props.onChange.call(this, {target:{value, name: this.props.name}});
+  findMatches(search, options = this.props.options) {
+    return filter(options, (option) => this.matchesOption(search, option));
   }
 
-  findMatches(value) {
-    let {parts, search} = this.parseValue(value);
-    let options = this.props.options;
-
-    // If there is more than one part to the value, remove options that
-    // match the existing parts.
-    if (parts.length > 1) {
-      let chosenParts = parts.slice(0, -1);
-      options = filter(options, (option) => !chosenParts.includes(option.id));
+  matchesOption(search, option) {
+    search = search.toLowerCase();
+    for (let key in option) {
+      if (option[key].toLowerCase().includes(search)) return true;
     }
-
-    // An empty string matches all options.
-    if (!search) return options;
-
-    return filter(options, function(option) {
-      for (let key in option) {
-        if (includes(option[key], search)) return true;
-      }
-    });
   }
 
-  parseValue(value) {
-    let parts = value.split(',');
-    let search = parts[parts.length - 1];
-    return {value, parts, search};
+  buildOptionsMap(options) {
+    this.optionsMap = {};
+    for (let option of options) {
+      this.optionsMap[option.id] = option;
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !(
+      nextState.value === this.state.value &&
+      nextState.matches === this.state.matches &&
+      nextState.selectedMatchIndex === this.state.selectedMatchIndex &&
+      nextState.open === this.state.open
+    );
   }
 
   render() {
 
     let className = 'SearchSuggest';
-    if (this.state.showMatches) className += ' SearchSuggest--open';
+    if (this.state.open && this.state.matches.length) {
+      className += ' SearchSuggest--open';
+    }
 
     return (
       <div className={className}>
@@ -251,7 +237,10 @@ export default class SearchSuggest extends React.Component {
           autoCapitalize="off"
           spellCheck="false"
           ref="input" />
-        <ul className="SearchSuggest-matches" ref="matches">
+        <ul
+          className="SearchSuggest-matches"
+          onScroll={this.handleScroll.bind(this)}
+          ref="matches">
           {this.state.matches.map((match, index) => {
             let className = 'SearchSuggestMatch';
             if (this.state.selectedMatchIndex == index) {
@@ -262,7 +251,6 @@ export default class SearchSuggest extends React.Component {
                 className={className}
                 onMouseEnter={this.handleMouseEnter.bind(this, index)}
                 onClick={this.setSelectedMatchState.bind(this, match.id)}
-                ref={match.id}
                 key={match.id}>
                 <span className="SearchSuggestMatch-category">
                   {match.group}
@@ -287,17 +275,3 @@ SearchSuggest.defaultProps = {
   value: '',
   options: []
 };
-
-
-// Match against the lowercase version and replace all 1-2 digit number
-// values with "XX" to match things like ga:dimensionXX.
-function matches(choice, search) {
-  return search.replace(/\d{1,2}/, 'XX').toLowerCase() == choice.toLowerCase();
-}
-
-// Match against the lowercase version and replace all 1-2 digit number
-// values with "XX" to match things like ga:dimensionXX.
-function includes(choice, search) {
-  return choice.toLowerCase()
-      .includes(search.replace(/\d{1,2}/, 'XX').toLowerCase());
-}
