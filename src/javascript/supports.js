74011990a80f9babeebe8fba0ac1d418fe2d3c70
Philip Walton <philip@philipwalton.com>
2015-06-29 19:02:20 -0700

    Update the copyToClipboard feature detection

--- src/javascript/supports.js
+++ src/javascript/supports.js
@@ -18,33 +18,35 @@
 
 export default {
 
+  /**
+   * Returns true if the browser supports copying to the clipboard.
+   * @return {boolean}
+   */
   copyToClipboard: (function() {
 
     let isCopySupported;
 
-    // TODO(philipwalton): At the moment, Firefox has a false positive on the
-    // `queryCommandSupported('copy')` method, so at the moment we simply have
-    // to exclude it. Once Firefox enables true support, remove this
-    // conditional.
-    // http://goo.gl/x4jcS
-    if (typeof InstallTrigger === 'undefined') {
-
-      // TODO(philipwalton): Chrome currently only returns true for the
-      // `queryCommandSupported('copy')` method if it's initiated by user
-      // interaction, so we have to listen for events and track it.
-      // https://goo.gl/6bkQ7T
-      $(document).on('click keydown touchstart', function fn(e) {
-        try {
-          isCopySupported = document.queryCommandSupported('copy');
-        } catch(e) {
-          isCopySupported = false;
-        }
-        $(document).off('click keydown touchstart', fn);
-      });
-    }
-
     return function() {
-      return isCopySupported;
+      return isCopySupported || (isCopySupported =
+
+        // Firefox has a false positive on the
+        // `queryCommandSupported('copy')` method, so at the moment we simply
+        // have to exclude it. Once Firefox enables true support, remove this
+        // conditional.
+        // http://goo.gl/x4jcS
+        typeof InstallTrigger === 'undefined' &&
+
+        // If queryCommandSupported returns true, assume it's correct since
+        // we're ignoring Firefox for now.
+        document.queryCommandSupported &&
+        document.queryCommandSupported('copy') ||
+
+        // If we get this far we have to take some more drastic measures to
+        // work around a bug in Chrome where currently it only returns true for
+        // the `queryCommandSupported('copy')` method if it's initiated by user
+        // interaction. https://goo.gl/6bkQ7T
+        /Chrome\/(\d+)/.test(navigator.userAgent) && (+RegExp.$1 >= 43)
+      );
     }
   }())
 
