6ad01b744e67f98ab09a5336b7187f7bd16c9567
mcohoon <mcohoon@gmail.com>
2016-07-08 14:25:45 -0700

    The resultsViewer now properly displays the cohort responses.

--- src/javascript/request-composer/components/cohort-table.js
+++ src/javascript/request-composer/components/cohort-table.js
@@ -8,8 +8,9 @@ export function createCohortData(report, settings) {
     return;
   }
 
-  var headers = [];
+  var headers = ['Cohort'];
   var cohortTable = [];
+  var cohortRows = [];
 
     switch(settings.responseCohortSize) {
       case 'Day':
@@ -17,25 +18,11 @@ export function createCohortData(report, settings) {
         for(var i = 0; i < 7; i++) {
           headers.push('Day ' + i);
         }
-        for(var i = 0; i < 7; i++) {
-          var cohortRow = {};
-          for (var j = 0, header; header = headers[j]; ++j) {
-            cohortRow[header] = Math.floor(Math.random() * (100 - 0)) + 0;
-          }
-          cohortTable.push(cohortRow);
-        }
         break;
       case 'Week':
         // Create cohorts for the past 6 weeks.
         for(var i = 0; i < 6; i++) {
           headers.push('Week ' + i);
-        }
-        for(var i = 0; i < 6; i++) {
-          var cohortRow = {};
-          for (var j = 0, header; header = headers[j]; ++j) {
-            cohortRow[header] = Math.floor(Math.random() * (100 - 0)) + 0;
-          }
-          cohortTable.push(cohortRow);
         }
         break;
       case 'Month':
@@ -43,23 +30,28 @@ export function createCohortData(report, settings) {
         for(var i = 0; i < 3; i++) {
           headers.push('Month ' + i);
         }
-        for(var i = 0; i < 7; i++) {
-          var cohortRow = {};
-          for (var j = 0, header; header = headers[j]; ++j) {
-            cohortRow[header] = Math.floor(Math.random() * (100 - 0)) + 0;
-          }
-          cohortTable.push(cohortRow);
-        }
         break;
     }
 
-for (var j = 0, header; header = headers[j]; ++j) {
+  if (report.data.rows && report.data.rows.length) {
+    for (var rowIndex=0, row; row = report.data.rows[rowIndex]; ++rowIndex) {
+      if (row.dimensions && row.dimensions.length == 2) {
+        var cohortRowKey = row.dimensions[0];
+        var cohortValueKey = settings.responseCohortSize + ' ' + Number(row.dimensions[1]);
+        if (cohortRows.indexOf(cohortRowKey) == -1) {
+          cohortRows.push(cohortRowKey);
+          cohortTable.push({'Cohort': cohortRowKey});
+        }
+        cohortTable[cohortTable.length-1][cohortValueKey] = row.metrics[0].values[0];
+      }
+    }
+  }
 
-}
+  console.log(cohortTable);
 
   return {
       cohortTable: cohortTable,
-      headers: headers
+      headers: headers,
   };
 };
 
@@ -80,17 +72,17 @@ export default class CohortTable extends React.Component {
       <Table
         rowsCount={data.cohortTable.length}
         rowHeight={50}
-        headerHeight={100}
+        headerHeight={50}
         width={1000}
         height={500}>
         {data.headers.map((header) => (
           <Column
             fixed={true}
             header={<Cell>{header}</Cell>}
+            width={200}
             cell={props => (
                 <Cell {...props}>{data.cohortTable[props.rowIndex][header]}</Cell>
               )}
-            width={150}
           />
         ))}
       </Table>
