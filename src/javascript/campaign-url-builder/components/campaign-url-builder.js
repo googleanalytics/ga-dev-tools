af0124b51e7d18e4c4af5b133201fde99da0dabc
Philip Walton <philip@philipwalton.com>
2016-09-21 14:52:58 -0700

    Remove citysearch for the example list

--- src/javascript/campaign-url-builder/components/campaign-url-builder.js
+++ src/javascript/campaign-url-builder/components/campaign-url-builder.js
@@ -137,8 +137,7 @@ export default class CampaignUrlBuilder extends React.Component {
               <div className="FormControl-info">
                 The referrer: (e.g.&nbsp;
                   <code>google</code>,&nbsp;
-                  <code>citysearch</code>,&nbsp;
-                  <code>newsletter4</code>)
+                  <code>newsletter</code>)
               </div>
             </div>
           </div>
