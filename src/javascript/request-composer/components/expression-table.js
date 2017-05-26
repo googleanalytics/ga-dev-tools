07ca92a9173eb04566ac339004a5a6b9fadbb333
Matthew Cohoon <mcohoon@google.com>
2017-05-26 13:41:44 -0700

    Initial commit of expression-table.js, a table to display the results of metric expressions requests.

---
+++ src/javascript/request-composer/components/expression-table.js
@@ -0,0 +1,219 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+import uniq from 'lodash/uniq';
+import React from 'react';
+import metadata from 'javascript-api-utils/lib/metadata';
+
+let columns;
+window.addEventListener('load', () => {
+  metadata.get().then((c) => {
+    columns = c;
+  });
+});
+
+
+/**
+ * Accepts a string value and API type and formats it accordingly.
+ * @param {string} value
+ * @param {string} type
+ * @return {string} The formatted value.
+ */
+function formatValue(value, type) {
+  if (type == 'PERCENT') {
+    value = (Math.round(value * 100) / 100) + '%';
+  } else if (type == 'CURRENCY') {
+    value = '$' + (+value).toFixed(2);
+  }
+  return value;
+}
+
+
+/**
+ * Accepts a string dimension value and the name of a dimensions and formats
+ * the value if necessary.
+ * @param {string} value
+ * @param {string} name
+ * @return {string} The formatted value.
+ */
+function formatDimension(value, name) {
+  if (name == 'ga:date') {
+    value = `${value.slice(0, 4)}-${value.slice(4, 6)}-${value.slice(6)}`;
+  }
+  return value;
+}
+
+
+/**
+ * Accepts the report object from the API response and returns a
+ * two-dimensional array representing the table rows and cells.
+ * @param {Object} report The API response's report object.
+ * @return {Array}
+ */
+function constructTableCellFromReport(report) {
+  // Create local aliases.
+  let {columnHeader, data: {rows, totals}} = report;
+
+  if (!rows) return;
+
+  let {metricHeaderEntries = []} = columnHeader.metricHeader;
+
+  //let {pivotHeaderEntries = []} = columnHeader.metricHeader.pivotHeaders[0];
+  let {dimensions} = columnHeader;
+  //let pivotDimensions = pivotHeaderEntries[0].dimensionNames;
+
+  //let dimensionHeaderEndRow = dimensions.length - 1;
+  let metricHeaderRow = 0;//dimensionHeaderEndRow + 1;
+  let resultsStartRow = metricHeaderRow + 1;
+  let resultsEndRow = rows.length - 1;
+  //let totalsRow = resultsEndRow + 1;
+
+  let dimensionEndCol = dimensions.length - 1;
+  let metricStartCol = 1;// dimensionEndCol + 1;
+  let metricEndCol = metricStartCol + metricHeaderEntries.length - 1;
+  //let pivotStartCol = metricEndCol + 1;
+  //let pivotEndCol = pivotStartCol + pivotHeaderEntries.length - 1;
+
+  let rowCount = rows.length + 1;
+  let colCount = dimensions.length + 1;
+
+  // Creates the initial list of cells.
+  // This will be a two-dimensional array: cells[<tr>][<td>].
+  let cells = [];
+
+  // Adds empty cells in the upper left corner.
+  for (let r = 0; r < rowCount; r++) {
+    cells[r] = cells[r] || [];
+
+    for (let c = 0; c < colCount; c++) {
+      // Creates a new cell object.
+      let cell = cells[r][c] = {r, c, classes: [], text: ''};
+
+
+      if (r === metricHeaderRow) {
+        // Metric header rows
+        cell.isHeader = true;
+
+        // Primary metric column headers.
+        if (c >= metricStartCol && c <= metricEndCol) {
+          let metric = metricHeaderEntries[c - metricStartCol].name;
+          cell.classes.push('PivotTable-metricHeader');
+          cell.text = columns.get(metric).uiName;
+        }
+      } if (r === metricHeaderRow) {
+        // Result rows
+        let row = rows[r];
+
+        // Dimension results.
+        if (c <= dimensionEndCol) {
+          let dimensionName = dimensions[c];
+          console.log(row);
+          let dimensionValue = row.dimensions[c];
+          cell.isHeader = true;
+          cell.classes.push('PivotTable-dimensionHeader');
+          if (c === dimensionEndCol) {
+            cell.classes.push('PivotTable-dimensionHeader--lastCol');
+          }
+          if (r === resultsEndRow) {
+            cell.classes.push('PivotTable-dimensionHeader--lastRow');
+          }
+
+          cell.text = formatDimension(dimensionValue, dimensionName);
+          cell.dimensions = row.dimensions.slice(0, c + 1).join(',');
+        } else if (c >= metricStartCol && c <= metricEndCol) {
+          // Primary metric results.
+          let value = row.metrics[0].values[c - metricStartCol];
+          let type = metricHeaderEntries[c - metricStartCol].type;
+          cell.classes.push('PivotTable-value');
+          if (r === resultsEndRow) {
+            cell.classes.push('PivotTable-value--lastRow');
+          }
+          cell.text = formatValue(value, type);
+        }
+      } else {
+        // Totals row
+        // The totals label under the dimension row headers.
+        if (c <= dimensionEndCol) {
+          cell.isHeader = true;
+          cell.text = cell.dimensions = 'Totals';
+          cell.classes.push('PivotTable-total');
+        } else if (c >= metricStartCol && c <= metricEndCol) {
+          // Primary metric totals.
+          let value = totals[0].values[c - metricStartCol];
+          let type = metricHeaderEntries[c - metricStartCol].type;
+          cell.classes.push('PivotTable-total');
+          cell.text = formatValue(value, type);
+        }
+      }
+    }
+  }
+
+  return cells;
+}
+
+/**
+ * A components that renders the pivot table visualization.
+ */
+export default class ExpressionTable extends React.Component {
+
+  /**
+   * React lifecycyle method below:
+   * http://facebook.github.io/react/docs/component-specs.html
+   * ---------------------------------------------------------
+   */
+
+
+  /** @return {Object} The React component. */
+  render() {
+    let {result} = this.props.response;
+
+    if (result) {
+      let report = result.reports[0];
+      let cells = constructTableCellFromReport(report);
+
+      if (cells) {
+        return (
+          <div className="PivotTable">
+            <table className="PivotTable-table">
+              <tbody>
+                {cells.map((row, r) => (
+                 <tr className="PivotTable-tr" key={r}>
+                    {row.map((cell, c) => {
+                      if (cell.ref) return undefined;
+                      let tag = cell.isHeader ? 'th' : 'td';
+                      let props = {key: c};
+                      cell.classes.push(`PivotTable-${tag}`);
+
+                      if (cell.colSpan) props.colSpan = cell.colSpan;
+                      if (cell.rowSpan) props.rowSpan = cell.rowSpan;
+
+                      props.className = uniq(cell.classes).join(' ');
+
+                      return React.createElement(tag, props, cell.text);
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        );
+      } else {
+        return <p>No data was returned for this request.</p>;
+      }
+    } else {
+      return null;
+    }
+  }
+}
