84e56815fa00e4d7521e1c883068ed31e7b92c06
Nathan West <nathanwest@google.com>
2017-12-14 20:38:20 -0800

    Added test for URL testing functionality

---
+++ test/campaign-url-builder/problematic.js
@@ -0,0 +1,70 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { expect } from 'chai'
+import renderProblematic from
+  '../../src/javascript/campaign-url-builder/components/problematic.js'
+
+// Given a basic domain, test the domain for all combinations of
+// leading protocol, subdomain, path, query, and fragment.
+const thoroughlyTestDomain = (domain, test) =>
+  ["", "http://", "https://"].forEach(proto =>
+    ["", "www.", "play.google.com."].forEach(subdomain =>
+      ["", "/", "/object/path", "/trailing/slash/"].forEach(path =>
+        ["", "?utm_content=test"].forEach(query =>
+          ["", "#fragment"].forEach(fragment =>
+            test(`${proto}${subdomain}${domain}${path}${query}${fragment}`)
+          )
+        )
+      )
+    )
+  )
+
+describe('campaign-url-builder', () => {
+  describe('problematic URL detection', () => {
+    describe('acceptable URLs', () => {
+      ['example.com', 'mail.google.com'].forEach(domain =>
+        thoroughlyTestDomain(domain, url =>
+          it(`doesn't flag this url as problematic: "${url}"`, () => {
+            const renderResult = renderProblematic(url)
+            expect(renderResult).to.be.null
+          })
+        )
+      )
+    })
+
+    describe('problematic URLs', () => {
+      [
+        'play.google.com',
+        'itunes.apple.com',
+        'ga-dev-tools.appspot.com'
+      ].forEach(domain => thoroughlyTestDomain(domain, url =>
+        it(`flags this url as problematic: "${url}"`, () => {
+          const renderResult = renderProblematic(url)
+          expect(renderResult).to.not.be.null
+          // TODO(nathanwest): Test that the returned React element includes
+          // a url to the correct url builder. This requires introspecting
+          // React elements, which I don't know how to do yet.
+        })
+      ))
+    })
+
+    describe('performance', () => {
+      // TODO(nathanwest): during development, there was a major
+      // performance issue related to a pathological regex. Test that
+      // it doesn't take 3 seconds to match a 10 character string.
+      // Need to review JS performance testing libraries.
+    })
+  })
+})
