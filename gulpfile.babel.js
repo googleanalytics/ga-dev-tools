c6361a76a0c2ad6ee71670b9c99556ebc7d4b667
Nathan West <nathanwest@google.com>
2018-02-06 18:21:23 -0800

    re-re-re-indented things

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -249,11 +249,11 @@ gulp.task('json', function() {
 
 
 gulp.task('keycheck', () => fs.access('./service-account-key.json').catch(
-  error => Promise.reject(error +
-    '\nNeed a service account key. See ' +
-    'https://ga-dev-tools.appspot.com/embed-api/server-side-authorization/ ' +
-    'for details on how to get one.'
-  )
+    error => Promise.reject(error +
+        '\nNeed a service account key. See ' +
+        'https://ga-dev-tools.appspot.com/embed-api/server-side-authorization/ ' +
+        'for details on how to get one.'
+    )
 ));
 
 
