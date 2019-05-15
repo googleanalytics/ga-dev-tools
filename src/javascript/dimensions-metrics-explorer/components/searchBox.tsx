9aa27725d213c058a22817ac590bf3a6ef9d8789
Nathan West <nathanwest@google.com>
2019-05-14 17:04:56 -0700

    WIP commit

--- src/javascript/dimensions-metrics-explorer/components/searchBox.tsx
+++ src/javascript/dimensions-metrics-explorer/components/searchBox.tsx
@@ -2,43 +2,6 @@ import * as React from 'react'
 
 import Icon from '../../components/icon';
 
-const SearchText: React.FC<{
-  searchText: string,
-  setSearchText: (text: string) => void,
-}> = ({
-  searchText, setSearchText
-}) => {
-  const setText = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value),
-    [setSearchText]
-  );
-
-  const clearText = React.useCallback(
-    () => setSearchText(""),
-    [setSearchText]
-  );
-
-  return <div className="FormControl FormControl--inline">
-    <label className="FormControl-label">Search</label>
-    <div className="FormControl-body">
-      <div className="FlexLine">
-        <input
-          className={"FormField FormFieldAddOn-field"}
-          value={searchText}
-          onChange={setText} />
-        <button
-          type="button"
-          className="FormFieldAddOn-item"
-          onClick={clearText}
-          title={"Clear search term"}
-          tabIndex={-1}>
-          <Icon type="close" />
-        </button>
-      </div>
-    </div>
-  </div>
-}
-
 const SearchBox: React.FC<{
   searchText: string,
   onlySegments: boolean,
@@ -51,9 +14,45 @@ const SearchBox: React.FC<{
   searchText, onlySegments, allowDeprecated,
   setSearchText, setOnlySegments, setAllowDeprecated,
 }) => {
-  return <form>
-    <SearchText searchText={searchText} setSearchText={setSearchText}/>
-  </form>
-}
+  const setText = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value),
+    [setSearchText]
+  );
 
-export default SearchBox;
+  const clearText = React.useCallback(
+    () => setSearchText(""),
+    [setSearchText]
+  );
+
+  return <form>
+    <div className="FormControl FormControl--inline">
+      <label className="FormControl-label">Search</label>
+      <div className="FormControl-body">
+        <div className="FlexLine">
+          <input
+            className="FormField FormFieldAddOn-field"
+            value={searchText}
+            onChange={setText} />
+          <button
+            type="button"
+            className="FormFieldAddOn-item"
+            onClick={clearText}
+            title="Clear search term"
+            tabIndex={-1}>
+            <Icon type="close" />
+          </button>
+        </div>
+        <div className="FormControl-info">
+          <label>
+            <input
+              className="Checkbox"
+              type="checkbox"
+              onChange={set}
+              checked={!!settings.useDefinition} />
+            Show segment definitions instead of IDs.
+          </label>
+        </div>
+      </div>
+    </div>
+   </form>
+}
