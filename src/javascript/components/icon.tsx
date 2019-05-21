38f5da372728b05878f74bd3d235cf005b26e506
Nathan West <nathanwest@google.com>
2019-05-20 17:26:49 -0700

    Rewrite icon.tsx

--- src/javascript/components/icon.tsx
+++ src/javascript/components/icon.tsx
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-import React from 'react';
+import * as React from "react";
 
 // There is a known issue in React where for some reason a `use` element
 // is not compatible with the render cycle, because somehow the Shadow
@@ -24,41 +23,72 @@ import React from 'react';
 // and http://sfdcinpractice.com/index.php/2016/12/29/svg-icon-disappears-rerendering/
 // for details
 
-//let counter = 0
-
-type IconType = "add-circle" |
-    "arrow-back" |
-    "arrow-forward" |
-    "call-made" |
-    "check" |
-    "content-paste" |
-    "close" |
-    "error-outline" |
-    "event" |
-    "file-download" |
-    "home" |
-    "info-outline" |
-    "link" |
-    "menu" |
-    "person" |
-    "remove-circle" |
-    "refresh" |
-    "send" |
-    "warning" |
-    "bitly-logo";
+type IconType =
+  | "add-circle"
+  | "arrow-back"
+  | "arrow-forward"
+  | "call-made"
+  | "check"
+  | "content-paste"
+  | "close"
+  | "error-outline"
+  | "event"
+  | "file-download"
+  | "home"
+  | "info-outline"
+  | "link"
+  | "menu"
+  | "person"
+  | "remove-circle"
+  | "refresh"
+  | "send"
+  | "warning"
+  | "bitly-logo";
 
 /**
  * A components that renders an <svg> icon from the iconset in:
  * src/images/icons.svg
  */
-const Icon: React.FC<{type: IconType}> = ({type}) => (
-    <svg className="Icon" viewBox="0 0 24 24">
-          <use key={0} href={`/public/images/icons.svg#icon-${type}`} />
-    </svg>
-);
+const Icon: React.FC<{ type: IconType }> = React.memo(({ type }) => {
+  /**
+   * This component should simply render:
+   *
+   * <svg className="Icon" viewBox="0 0 24 24">
+   *     <use xlinkHref={`/public/images/icons.svg#icon-${type}`} />
+   * </svg>
+   *
+   * However, there is a known issue in React where for some reason an
+   * svg `<use>` element is not compatible with the render cycle, because
+   * somehow the Virtual DOM can't reconcile it when there are complex
+   * changes in the tree. We therefore use this manual dom element
+   * manipulaton effect to account for when `props.type` changes.
+   *
+   * See https://stackoverflow.com/questions/50771280/react-svg-disappearing-when-component-rerenders
+   * and http://sfdcinpractice.com/index.php/2016/12/29/svg-icon-disappears-rerendering/
+   * for details
+   */
 
-/*
+  const [svgNode, setSvgNode] = React.useState<SVGElement | null>(null);
 
-*/
+  React.useEffect(() => {
+    if (svgNode === null) {
+      return;
+    }
 
-export default Icon
+    const url = `/public/images/icons.svg#icon-${type}`;
+    const useNode = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "use"
+    );
+    useNode.setAttributeNS("http://www.w3.org/1999/xlink", "href", url);
+    svgNode.appendChild(useNode);
+
+    return () => {
+      useNode.remove();
+    };
+  }, [type, svgNode]);
+
+  return <svg className="Icon" viewBox="0 0 24 24" ref={setSvgNode} />;
+});
+
+export default Icon;
