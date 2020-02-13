5d29264fffe1ee8cd0c660c9b2dcf80d6d53ec81
Matt Hamrick <matthewjhamrick@gmail.com>
2020-02-13 22:18:33 +0000

    hit builder now is a functional component.

--- src/javascript/hit-builder/components/hit-builder.tsx
+++ src/javascript/hit-builder/components/hit-builder.tsx
@@ -43,144 +43,123 @@ export interface HitBuilderProps {
   properties: Property[];
 }
 
-/**
- * The primary Hit Builder app component.
- */
-export default class HitBuilder extends React.Component<HitBuilderProps> {
-  newParamNeedsFocus_?: boolean;
-  /**
-   * Adds a new param after a user clicks to the "Add parameter" button.
-   * Also turns on a flag to indicate that this param needs focus after
-   * rendering.
-   */
-  handleAddParam = () => {
-    this.newParamNeedsFocus_ = true;
-    this.props.actions.addParam();
-  };
+const HitBuilder: React.FC<HitBuilderProps> = ({
+  actions,
+  hitStatus,
+  validationMessages,
+  params,
+  properties
+}) => {
+  const [newParamNeedsFocus, setNewParamNeedsFocus] = React.useState(false);
 
-  /**
-   * Generates a random UUID value for the "cid" parameter.
-   */
-  handleGenerateUuid = () => {
-    this.props.actions.editParamValue(this.props.params[3].id, uuid.v4());
-  };
+  // Not sure If we need this.
+  React.useEffect(() => {
+    setNewParamNeedsFocus(false);
+  }, []);
 
-  /**
-   * Accepts a param and returns a validation message (if one exits) for the
-   * passed param.
-   * @param {Object} param
-   * @return {?Object}
-   */
-  getValidationMessageForParam(param) {
-    const message = this.props.validationMessages.find(m => m.param === param);
-    return message && message.description;
-  }
+  const getValidationMessageForParam = React.useCallback(
+    (paramName: string) => {
+      const message = validationMessages.find(m => m.param === paramName);
+      return message && message.description;
+    },
+    [validationMessages]
+  );
 
-  /**
-   * React lifecycyle methods below:
-   * http://facebook.github.io/react/docs/component-specs.html
-   * ---------------------------------------------------------
-   */
+  const handleGenerateUuid = React.useCallback(() => {
+    actions.editParamValue(params[3].id, uuid.v4());
+  }, [params]);
 
-  /** Forces focus on the newly created param. */
-  componentDidUpdate() {
-    this.newParamNeedsFocus_ = false;
-  }
+  const handleAddParam = React.useCallback(() => {
+    setNewParamNeedsFocus(true);
+    actions.addParam();
+  }, [actions]);
+  return (
+    <div>
+      <div className="HeadingGroup HeadingGroup--h3">
+        <h3>Hit summary</h3>
+        <p>
+          The box below displays the full hit and its validation status. You can
+          update the hit in the text box and the parameter details below will be
+          automatically updated.
+        </p>
+      </div>
 
-  /** @return {Object} The React component. */
-  render() {
-    const { params } = this.props;
+      <HitElement
+        actions={actions}
+        hitStatus={hitStatus}
+        validationMessages={validationMessages}
+        hitPayload={convertParamsToHit(params)}
+      />
 
-    return (
-      <div>
+      <div className="HitBuilderParams">
         <div className="HeadingGroup HeadingGroup--h3">
-          <h3>Hit summary</h3>
+          <h3>Hit parameter details</h3>
           <p>
-            The box below displays the full hit and its validation status. You
-            can update the hit in the text box and the parameter details below
-            will be automatically updated.
+            The fields below are a breakdown of the individual parameters and
+            values for the hit in the text box above. When you update these
+            values, the hit above will be automatically updated.
           </p>
         </div>
 
-        <HitElement
-          actions={this.props.actions}
-          hitStatus={this.props.hitStatus}
-          validationMessages={this.props.validationMessages}
-          hitPayload={convertParamsToHit(params)}
+        <ParamSelectElement
+          actions={actions}
+          param={params[0]}
+          options={["1"]}
+          message={getValidationMessageForParam("v")!}
         />
 
-        <div className="HitBuilderParams">
-          <div className="HeadingGroup HeadingGroup--h3">
-            <h3>Hit parameter details</h3>
-            <p>
-              The fields below are a breakdown of the individual parameters and
-              values for the hit in the text box above. When you update these
-              values, the hit above will be automatically updated.
-            </p>
-          </div>
+        <ParamSelectElement
+          actions={actions}
+          param={params[1]}
+          options={HIT_TYPES}
+          message={getValidationMessageForParam("t")!}
+        />
 
-          <ParamSelectElement
-            ref="v"
-            actions={this.props.actions}
-            param={params[0]}
-            options={["1"]}
-            message={this.getValidationMessageForParam("v")!}
-          />
+        <ParamSearchSuggestElement
+          actions={actions}
+          param={params[2]}
+          options={properties}
+          placeholder="UA-XXXXX-Y"
+          message={getValidationMessageForParam("tid")!}
+        />
 
-          <ParamSelectElement
-            ref="t"
-            actions={this.props.actions}
-            param={params[1]}
-            options={HIT_TYPES}
-            message={this.getValidationMessageForParam("t")!}
-          />
+        <ParamButtonElement
+          actions={actions}
+          param={params[3]}
+          type="refresh"
+          title="Randomly generate UUID"
+          message={getValidationMessageForParam("cid")!}
+          onClick={handleGenerateUuid}
+        />
 
-          <ParamSearchSuggestElement
-            ref="tid"
-            actions={this.props.actions}
-            param={params[2]}
-            options={this.props.properties}
-            placeholder="UA-XXXXX-Y"
-            message={this.getValidationMessageForParam("tid")!}
-          />
+        {params.slice(4).map(param => {
+          const isLast = param === params[params.length - 1];
+          return (
+            <ParamElement
+              key={param.id}
+              actions={actions}
+              param={param}
+              needsFocus={isLast && newParamNeedsFocus}
+              message={getValidationMessageForParam(param.name)}
+              onRemove={() => actions.removeParam(param.id)}
+            />
+          );
+        })}
 
-          <ParamButtonElement
-            ref="cid"
-            actions={this.props.actions}
-            param={params[3]}
-            type="refresh"
-            title="Randomly generate UUID"
-            message={this.getValidationMessageForParam("cid")!}
-            onClick={this.handleGenerateUuid}
-          />
-
-          {params.slice(4).map(param => {
-            const isLast = param === params[params.length - 1];
-            return (
-              <ParamElement
-                key={param.id}
-                actions={this.props.actions}
-                param={param}
-                needsFocus={isLast && this.newParamNeedsFocus_}
-                message={this.getValidationMessageForParam(param.name)}
-                onRemove={() => this.props.actions.removeParam(param.id)}
-              />
-            );
-          })}
-
-          <div className="HitBuilderParam HitBuilderParam--action">
-            <div className="HitBuilderParam-body">
-              <IconButton
-                type="add-circle"
-                iconStyle={{ color: "hsl(150,60%,40%)" }}
-                onClick={this.handleAddParam}
-              >
-                Add parameter
-              </IconButton>
-            </div>
+        <div className="HitBuilderParam HitBuilderParam--action">
+          <div className="HitBuilderParam-body">
+            <IconButton
+              type="add-circle"
+              iconStyle={{ color: "hsl(150,60%,40%)" }}
+              onClick={handleAddParam}
+            >
+              Add parameter
+            </IconButton>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default HitBuilder;
