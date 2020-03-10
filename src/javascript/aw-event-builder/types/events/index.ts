3141b3e8ce45f3076fe02ac8feab4ae13338a8a0
Matt Hamrick <matthewjhamrick@gmail.com>
2020-03-10 11:43:31 -0700

    added jobs, edu, etc event category.

--- src/javascript/aw-event-builder/types/events/index.ts
+++ src/javascript/aw-event-builder/types/events/index.ts
@@ -8,8 +8,53 @@ export * from "./empty-event";
 export enum MPEventCategory {
   Custom = "Custom",
   AllApps = "All apps",
-  RetailEcommerce = "Retail/Ecommerce"
+  RetailEcommerce = "Retail/Ecommerce",
+  Jobs_Edu_LocalDeails_RealEstate = "Jobs, Education, Local Deals, Real Estate"
 }
+
+export enum MPEventType {
+  // Custom Event
+  CustomEvent = "custom_event",
+  // All Apps
+  EarnVirtualCurrency = "earn_virtual_currency",
+  JoinGroup = "join_group",
+  Login = "login",
+  PresentOffer = "present_offer",
+  // Also Retail/Ecommerce
+  Purchase = "purchase",
+  Refund = "refund",
+  Search = "search",
+  SelectContent = "select_content",
+  Share = "share",
+  SignUp = "sign_up",
+  SpendVirtualCurrency = "spend_virtual_currency",
+  TutorialBegin = "tutorial_begin",
+  TutorialComplete = "tutorial_complete",
+  // Retail/Ecommerce
+  // https://support.google.com/firebase/answer/6317499?hl=en&ref_topic=6317484
+  // and (they are currently the same)
+  // Jobs, Education, Local Deals, Real Estate
+  // https://support.google.com/firebase/answer/6375140?hl=en&ref_topic=6317484
+  AddPaymentInfo = "add_payment_info",
+  AddToCart = "add_to_cart",
+  AddToWishlist = "add_to_wishlist",
+  BeginCheckout = "begin_checkout",
+  EcommercePurchase = "ecommerce_purchase",
+  GenerateLead = "generate_lead",
+  PurchaseRefund = "purchase_refund",
+  ViewItem = "view_item",
+  ViewItemList = "view_item_list",
+  ViewSearchResults = "view_search_results"
+}
+
+// Events: Custom
+interface CustomEvent {
+  type: MPEventType.CustomEvent;
+  parameters: {};
+  customParameters: Parameters;
+}
+
+export type MPEventData = CustomEvent | AllAppsEvent | RetailEcommerceEvent;
 
 export const eventTypesFor = (category: MPEventCategory): MPEventType[] => {
   switch (category) {
@@ -45,45 +90,19 @@ export const eventTypesFor = (category: MPEventCategory): MPEventType[] => {
         MPEventType.ViewItemList,
         MPEventType.ViewSearchResults
       ];
+    case MPEventCategory.Jobs_Edu_LocalDeails_RealEstate:
+      return [
+        MPEventType.AddPaymentInfo,
+        MPEventType.AddToCart,
+        MPEventType.AddToWishlist,
+        MPEventType.BeginCheckout,
+        MPEventType.EcommercePurchase,
+        MPEventType.GenerateLead,
+        MPEventType.Purchase,
+        MPEventType.PurchaseRefund,
+        MPEventType.ViewItem,
+        MPEventType.ViewItemList,
+        MPEventType.ViewSearchResults
+      ];
   }
 };
-
-export enum MPEventType {
-  // Custom Event
-  CustomEvent = "custom_event",
-  // All Apps
-  EarnVirtualCurrency = "earn_virtual_currency",
-  JoinGroup = "join_group",
-  Login = "login",
-  PresentOffer = "present_offer",
-  // Also Retail/Ecommerce
-  Purchase = "purchase",
-  Refund = "refund",
-  Search = "search",
-  SelectContent = "select_content",
-  Share = "share",
-  SignUp = "sign_up",
-  SpendVirtualCurrency = "spend_virtual_currency",
-  TutorialBegin = "tutorial_begin",
-  TutorialComplete = "tutorial_complete",
-  // Retail/Ecommerce
-  AddPaymentInfo = "add_payment_info",
-  AddToCart = "add_to_cart",
-  AddToWishlist = "add_to_wishlist",
-  BeginCheckout = "begin_checkout",
-  EcommercePurchase = "ecommerce_purchase",
-  GenerateLead = "generate_lead",
-  PurchaseRefund = "purchase_refund",
-  ViewItem = "view_item",
-  ViewItemList = "view_item_list",
-  ViewSearchResults = "view_search_results"
-}
-
-// Events: Custom
-interface CustomEvent {
-  type: MPEventType.CustomEvent;
-  parameters: {};
-  customParameters: Parameters;
-}
-
-export type MPEventData = CustomEvent | AllAppsEvent | RetailEcommerceEvent;
