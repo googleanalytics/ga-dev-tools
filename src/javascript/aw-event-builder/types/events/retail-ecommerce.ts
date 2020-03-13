599e47e73251391fe24307673c86738bab4a902f
Matt Hamrick <matthewjhamrick@gmail.com>
2020-03-13 10:42:42 -0700

    did retail ecommerce events as well.

--- src/javascript/aw-event-builder/types/events/retail-ecommerce.ts
+++ src/javascript/aw-event-builder/types/events/retail-ecommerce.ts
@@ -1,5 +1,5 @@
-import { OptionalString, Parameters } from "./parameters";
-import { MPEventType } from "./index";
+import { OptionalString } from "./parameters";
+import { MPEventType, EventData } from "./index";
 
 export type RetailEcommerceEvent =
   | AddPaymentInfoEvent
@@ -13,90 +13,64 @@ export type RetailEcommerceEvent =
   | ViewItemListEvent
   | ViewSearchResultsEvent;
 
-interface AddPaymentInfoEvent {
-  type: MPEventType.AddPaymentInfo;
-  parameters: {};
-  customParameters: Parameters;
-}
-interface AddToCartEvent {
-  type: MPEventType.AddToCart;
-  parameters: {
-    quantity: OptionalString;
-    item_category: OptionalString;
-    item_name: OptionalString;
-    item_id: OptionalString;
-    item_location_id: OptionalString;
-    value: OptionalString;
-    price: OptionalString;
-    currency: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface AddToWishlistEvent {
-  type: MPEventType.AddToWishlist;
-  parameters: {
-    quantity: OptionalString;
-    item_category: OptionalString;
-    item_name: OptionalString;
-    item_id: OptionalString;
-    item_location_id: OptionalString;
-    value: OptionalString;
-    price: OptionalString;
-    currency: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface BeginCheckoutEvent {
-  type: MPEventType.BeginCheckout;
-  parameters: {
-    coupon: OptionalString;
-    currency: OptionalString;
-    value: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface EcommercePurchaseEvent {
-  type: MPEventType.EcommercePurchase;
-  parameters: {
-    coupon: OptionalString;
-    currency: OptionalString;
-    value: OptionalString;
-    tax: OptionalString;
-    shipping: OptionalString;
-    transation_id: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface GenerateLeadEvent {
-  type: MPEventType.GenerateLead;
-  parameters: {
-    value: OptionalString;
-    currency: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface PurchaseRefundEvent {
-  type: MPEventType.PurchaseRefund;
-  parameters: {
-    quantity: OptionalString;
-    value: OptionalString;
-    currency: OptionalString;
-    transaction_id: OptionalString;
-  };
-  customParameters: Parameters;
-}
-interface ViewItemEvent {
-  type: MPEventType.ViewItem;
-  parameters: { item_id: OptionalString; item_location_id: OptionalString };
-  customParameters: Parameters;
-}
-interface ViewItemListEvent {
-  type: MPEventType.ViewItemList;
-  parameters: { item_category: OptionalString };
-  customParameters: Parameters;
-}
-interface ViewSearchResultsEvent {
-  type: MPEventType.ViewSearchResults;
-  parameters: { search_term: OptionalString };
-  customParameters: Parameters;
-}
+type AddPaymentInfoEvent = EventData<MPEventType.AddPaymentInfo, never>;
+type AddToCartEvent = EventData<
+  MPEventType.AddToCart,
+  | OptionalString<"quantity">
+  | OptionalString<"item_category">
+  | OptionalString<"item_name">
+  | OptionalString<"item_id">
+  | OptionalString<"item_location_id">
+  | OptionalString<"value">
+  | OptionalString<"price">
+  | OptionalString<"currency">
+>;
+type AddToWishlistEvent = EventData<
+  MPEventType.AddToWishlist,
+  | OptionalString<"quantity">
+  | OptionalString<"item_category">
+  | OptionalString<"item_name">
+  | OptionalString<"item_id">
+  | OptionalString<"item_location_id">
+  | OptionalString<"value">
+  | OptionalString<"price">
+  | OptionalString<"currency">
+>;
+type BeginCheckoutEvent = EventData<
+  MPEventType.BeginCheckout,
+  | OptionalString<"coupon">
+  | OptionalString<"currency">
+  | OptionalString<"value">
+>;
+type EcommercePurchaseEvent = EventData<
+  MPEventType.EcommercePurchase,
+  | OptionalString<"coupon">
+  | OptionalString<"currency">
+  | OptionalString<"value">
+  | OptionalString<"tax">
+  | OptionalString<"shipping">
+  | OptionalString<"transation_id">
+>;
+type GenerateLeadEvent = EventData<
+  MPEventType.GenerateLead,
+  OptionalString<"value"> | OptionalString<"currency">
+>;
+type PurchaseRefundEvent = EventData<
+  MPEventType.PurchaseRefund,
+  | OptionalString<"quantity">
+  | OptionalString<"value">
+  | OptionalString<"currency">
+  | OptionalString<"transaction_id">
+>;
+type ViewItemEvent = EventData<
+  MPEventType.ViewItem,
+  OptionalString<"item_id"> | OptionalString<"item_location_id">
+>;
+type ViewItemListEvent = EventData<
+  MPEventType.ViewItemList,
+  OptionalString<"item_category">
+>;
+type ViewSearchResultsEvent = EventData<
+  MPEventType.ViewSearchResults,
+  OptionalString<"search_term">
+>;
