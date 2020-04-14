44186aeefd3ac0454a332c20ae5b4158c8d31273
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-14 19:27:45 +0000

    Migrated the Overview to react.

--- src/javascript/account-explorer/components/AccountExplorer.tsx
+++ src/javascript/account-explorer/components/AccountExplorer.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
 
 const AccountExplorer: React.FC = () => {
-  return <div>Account explorer!</div>;
+  return (
+    <>
+      <section>
+        <h2>Overview</h2>
+        <p>
+          Use this tool to search or browse through your accounts, properties,
+          and views, See what accounts you have access to, and find the IDs that
+          you need for the API or for another tool or service that integrates
+          with Google Analytics.
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default AccountExplorer;
