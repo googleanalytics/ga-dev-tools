00c8fd94fea91b8f295f826880cea5397fdb8557
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-15 23:06:20 +0000

    remove unused import.s

--- src/javascript/components/ViewSelector3.tsx
+++ src/javascript/components/ViewSelector3.tsx
@@ -3,10 +3,6 @@
 
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
