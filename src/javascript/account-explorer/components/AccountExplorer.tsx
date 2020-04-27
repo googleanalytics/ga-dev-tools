3545bf99b755d5de4c569d32dc95a9cf9eae3ae9
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-27 16:53:45 +0000

    Fixed caps typo.

--- src/javascript/account-explorer/components/AccountExplorer.tsx
+++ src/javascript/account-explorer/components/AccountExplorer.tsx
@@ -111,7 +111,7 @@ const AccountExplorer: React.FC = () => {
         <Typography variant="h4">Overview</Typography>
         <Typography variant="body1">
           Use this tool to search or browse through your accounts, properties,
-          and views, See what accounts you have access to, and find the IDs that
+          and views, see what accounts you have access to, and find the IDs that
           you need for the API or for another tool or service that integrates
           with Google Analytics.
         </Typography>
