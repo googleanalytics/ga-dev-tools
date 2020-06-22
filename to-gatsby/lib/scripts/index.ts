93e7fee629e8adf78f4a0881218ee6a539e5c760
Matt Hamrick <matthewjhamrick@gmail.com>
2020-06-22 20:20:27 +0000

    Added first pass at implementing the check for measurement id.

--- to-gatsby/lib/scripts/index.ts
+++ to-gatsby/lib/scripts/index.ts
@@ -1,8 +1,10 @@
 import * as path from "path"
 import * as fs from "fs"
 import * as argparse from "argparse"
+import * as inquirer from "inquirer"
 
 const PWD = process.cwd()
+const Encoding = "utf8"
 
 const RuntimeJsonPath = path.join(PWD, "runtime.json")
 
@@ -11,18 +13,85 @@ interface RuntimeJson {
   gaMeasurementIdDev: string
 }
 
-// Ensures that `runtime.json` exists. If it doesn't exist, prompts the user for
-// required values & creates it.
-const ensureRuntimeJson = async () => {
-  console.log(`The path is: ${PWD}`)
+type ConfigQuestionFilter = {
+  askAll?: true | undefined
+}
+
+const ensureNecessaryFiles = (runtimeJson: RuntimeJson): Promise<void> => {
+  // Create `runtime.json` if it doesn't exist.
   const exists = fs.existsSync(RuntimeJsonPath)
   if (!exists) {
-    console.log(
-      "You do not have a `runtime.json` file. \nPlease answer the following prompts to populate one:"
-    )
+    fs.writeFileSync(RuntimeJsonPath, "")
   }
+
+  // Overwrite `runtime.json` with provided configuration.
+  fs.writeFileSync(RuntimeJsonPath, JSON.stringify(runtimeJson, null, "  "), {
+    encoding: Encoding,
+  })
+
+  return
+}
+
+const configQuestions = (
+  filter: ConfigQuestionFilter
+): inquirer.QuestionCollection<RuntimeJson> => {
+  return [
+    {
+      name: "gaMeasurementId",
+      // TODO - Nice to have, list the user's available properties. Would
+      // require the user to authenticate first to make the request & could be
+      // optional.
+      type: "input",
+      message: "Measurement ID for production: ",
+      default: "none-provided",
+      when: () => {
+        return filter.askAll
+      },
+    },
+    {
+      name: "gaMeasurementIdDev",
+      // TODO - Nice to have, list the user's available properties. Would
+      // require the user to authenticate first to make the request & could be
+      // optional.
+      type: "input",
+      message: "Measurement ID for development: ",
+      default: "none-provided",
+      when: () => {
+        return filter.askAll
+      },
+    },
+  ]
+}
+
+// Ensures that required config files exist. If they don't, prompt the user for
+// required values & creates necessary files.
+const checkConfig = async () => {
+  console.log("Checking required configuration...")
+  const exists = fs.existsSync(RuntimeJsonPath)
+
+  let filter: ConfigQuestionFilter = {}
+
+  // If file doesn't exist at all, prompt for all configuration entries.
+  if (!exists) {
+    filter = { askAll: true }
+  }
+
+  // TODO if some of the values were already there, make sure to merge answers
+  // with the current values.
+  const answers = await inquirer.prompt(configQuestions(filter))
+
+  await ensureNecessaryFiles(answers)
+  console.log(answers)
   return true
 }
+
+interface CheckRuntimeFilesArgs {}
+
+enum Command {
+  CheckRequiredConfiguration = "check-config",
+}
+
+type Args = CheckRuntimeFilesArgs & { cmd: Command }
 
 const getParser = async (): Promise<argparse.ArgumentParser> => {
   const parser = new argparse.ArgumentParser({
@@ -30,18 +99,32 @@ const getParser = async (): Promise<argparse.ArgumentParser> => {
     addHelp: true,
     description: "Scripts for managing ga-dev-tools development.",
   })
+
+  const subparsers = parser.addSubparsers({
+    title: "Sub command",
+    dest: "cmd",
+  })
+
+  subparsers.addParser("check-config", {
+    help:
+      "Ensures that all necessary configuration files exist & have required values.",
+  })
+
   return parser
 }
 
 const scripts = async () => {
   const parser = await getParser()
-  const args = parser.parseArgs()
-  console.log(args)
+  const args = parser.parseArgs() as Args
 
-  console.log(
-    "Placeholder for generalized scripts that check for requirements & allow fixing of them before actually running."
-  )
-  await ensureRuntimeJson()
+  switch (args.cmd) {
+    case Command.CheckRequiredConfiguration: {
+      await checkConfig()
+      break
+    }
+  }
+
+  process.exit(0)
 }
 
 scripts()
