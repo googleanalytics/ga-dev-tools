43b1bdbaedfd2398c9c04800a170e24f77600ee0
Matt Hamrick <matthewjhamrick@gmail.com>
2021-01-07 11:02:47 -0800

    random stopping point. Going back to dimns and metrics now that I have some feedback.

--- to-gatsby/src/pages/query-explorer/index.tsx
+++ to-gatsby/src/pages/query-explorer/index.tsx
@@ -15,8 +15,71 @@
 import * as React from "react"
 
 import Layout from "../../components/layout"
+import { Typography, TextField, makeStyles } from "@material-ui/core"
+import { Url } from "../../constants"
+import ViewSelector, { HasView } from "../../components/ViewSelector"
 
-const QueryExplorer = () => {
-  return <Layout title="Query Explorer">TODO - add in content.</Layout>
+const coreReportingApi = <a href={Url.coreReportingApi}>Core Reporting API</a>
+
+const useStyles = makeStyles(theme => ({
+  inputs: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+  },
+}))
+
+export const QueryExplorer = () => {
+  const classes = useStyles()
+  const [selectedView, setSelectedView] = React.useState<HasView>(undefined)
+  const [view, setView] = React.useState("")
+
+  // When the selected view is changed, update the text box for view with the
+  // id.
+  React.useEffect(() => {
+    if (selectedView === undefined) {
+      return
+    }
+    const viewId = `ga:${selectedView.view.id}`
+    setView(viewId)
+  }, [selectedView])
+
+  return (
+    <>
+      <Typography variant="h2">Overview</Typography>
+      <Typography variant="body1">
+        Sometimes you just need to explore. This tool lets you play with the{" "}
+        {coreReportingApi} by building queries to get data from your Google
+        Analytics views (profiles). You can use these queries in any of the
+        client libraries to build your own tools.
+      </Typography>
+      <Typography variant="h3">Select View</Typography>
+      <ViewSelector onViewChanged={setSelectedView} />
+      <Typography variant="h3">Set query parameters</Typography>
+      <section className={classes.inputs}>
+        <TextField
+          id="ids"
+          value={view}
+          onChange={e => setView(e.target.value)}
+          label="ids"
+        />
+        <TextField id="start-date" label="start-date" />
+        <TextField id="end-date" label="end-date" />
+        <TextField id="metrics" label="metrics" />
+        <TextField id="dimensions" label="dimensions" />
+        <TextField id="sort" label="sort" />
+        <TextField id="filters" label="filters" />
+      </section>
+    </>
+  )
 }
-export default QueryExplorer
+
+const Wrapped = () => {
+  return (
+    <Layout title="Query Explorer">
+      <QueryExplorer />
+    </Layout>
+  )
+}
+export default Wrapped
