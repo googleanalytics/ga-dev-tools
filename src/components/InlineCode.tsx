0a973c8fd8b0a25d7b085391888cc8a868a140ba
Matt Hamrick <matthewjhamrick@gmail.com>
2021-04-19 12:05:58 -0700

    Put in temporary (but probably permanent) fix for InlineCode breaking SSR.

--- src/components/InlineCode.tsx
+++ src/components/InlineCode.tsx
@@ -1,30 +1,36 @@
 import * as React from "react"
-import { makeStyles, Typography } from "@material-ui/core"
-import classnames from "classnames"
+import { useTheme } from "@material-ui/core"
+// import { makeStyles } from "@material-ui/core"
 
-const useStyles = makeStyles(theme => ({
-  code: {
-    backgroundColor: theme.palette.grey[300],
-    color: theme.palette.getContrastText(theme.palette.grey[300]),
-    padding: theme.spacing(0.25, 0.5),
-    borderRadius: theme.spacing(0.25),
-    fontFamily: "'Source Code Pro', monospace",
-  },
-}))
+// TODO I'm not sure why, but if I use this makeStyles, it somehow conflicts
+// somewhere and messes up formatting during SSR. Good luck to anybody that
+// wants to try to chase down this problem. :)
+//
+// const useStyles = makeStyles(theme => ({
+//   code: {
+//     backgroundColor: theme.palette.grey[300],
+//     color: theme.palette.getContrastText(theme.palette.grey[300]),
+//     padding: theme.spacing(0.25, 0.5),
+//     borderRadius: theme.spacing(0.25),
+//     fontFamily: "'Source Code Pro', monospace",
+//   },
+// }))
 
-const InlineCode: React.FC<{ className?: string }> = ({
-  children,
-  className,
-}) => {
-  const classes = useStyles()
+const InlineCode: React.FC<{ className?: string }> = ({ children }) => {
+  const theme = useTheme()
+  // const classes = useStyles()
   return (
-    <Typography
-      className={classnames(classes.code, className)}
-      variant="body2"
-      component="span"
+    <span
+      style={{
+        backgroundColor: theme.palette.grey[300],
+        color: theme.palette.getContrastText(theme.palette.grey[300]),
+        padding: theme.spacing(0.25, 0.5),
+        borderRadius: theme.spacing(0.25),
+        fontFamily: "'Source Code Pro', monospace",
+      }}
     >
       {children}
-    </Typography>
+    </span>
   )
 }
 
