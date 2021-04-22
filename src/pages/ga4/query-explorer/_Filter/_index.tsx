8385c8e936ffe473f4bb099c2fbfbbdf6f080169
Matt Hamrick <matthewjhamrick@gmail.com>
2021-04-22 11:37:09 -0700

    extract out useFilter hook.

--- src/pages/ga4/query-explorer/_Filter/_index.tsx
+++ src/pages/ga4/query-explorer/_Filter/_index.tsx
@@ -4,10 +4,11 @@ import {
   GA4Dimension,
   GA4Metrics,
 } from "../../../../components/GA4Pickers"
-import { useState, useCallback } from "react"
-import Expression, { ExpressionType } from "./_Expression"
+import { useCallback } from "react"
+import Expression from "./_Expression"
 import { Typography, makeStyles } from "@material-ui/core"
 import { Dispatch } from "../../../../types"
+import useFilter from "./_useFilter"
 
 const useStyles = makeStyles(theme => ({
   filter: {
@@ -33,116 +34,10 @@ export type FilterExpression = gapi.client.analyticsdata.FilterExpression
 export type FilterExpressionList = gapi.client.analyticsdata.FilterExpressionList
 export type BaseFilter = NonNullable<FilterExpression["filter"]>
 
-const subFor = (type: ExpressionType): FilterExpression => {
-  switch (type) {
-    case ExpressionType.And:
-      return { andGroup: { expressions: [] } }
-    case ExpressionType.Or:
-      return { orGroup: { expressions: [] } }
-    case ExpressionType.Not:
-      return { notExpression: {} }
-    case ExpressionType.Filter:
-      return { filter: {} }
-  }
-}
-
 export type UpdateFilter = (
   path: (string | number)[],
   update: (old: BaseFilter) => BaseFilter
 ) => void
-
-const useFilter = () => {
-  const [expression, setExpression] = useState<FilterExpression>({})
-
-  const updateFilter = useCallback<UpdateFilter>(
-    (path, update) => {
-      setExpression(old => {
-        const cloned = { ...old }
-        const butLast = [...path]
-        let last = butLast.pop()
-
-        const navigated = butLast.reduce(
-          (ref, pathEntry) => ref[pathEntry],
-          cloned
-        )
-
-        navigated[last as any] = update(navigated[last as any])
-
-        console.log({ last, butLast, navigated })
-
-        return cloned
-      })
-    },
-    [setExpression]
-  )
-
-  const removeExpression = useCallback(
-    (path: (string | number)[]) => {
-      setExpression(old => {
-        const cloned = { ...old }
-        const butLast = [...path]
-        let last = butLast.pop()
-
-        if (
-          last === "andGroup" ||
-          last === "orGroup" ||
-          last === "notExpression"
-        ) {
-          last = butLast.pop()
-        }
-
-        // If there is no last, that means we're at the top level
-        if (last === undefined) {
-          return {}
-        }
-
-        const navigated = butLast.reduce(
-          (ref, pathEntry) => ref[pathEntry],
-          cloned
-        )
-
-        if (Array.isArray(navigated)) {
-          navigated.splice(last as number, 1)
-        }
-
-        navigated[last] = {}
-
-        return cloned
-      })
-    },
-    [setExpression]
-  )
-
-  const addExpression = useCallback(
-    (path: (string | number)[], type: ExpressionType) => {
-      setExpression(old => {
-        console.log(JSON.stringify(old, undefined, " "))
-        console.log("path", path)
-        const cloned = { ...old }
-        const butLast = [...path]
-        const last = butLast.pop()
-
-        const sub = subFor(type)
-
-        // If there is no last, that means we're at the top level
-        if (last === undefined) {
-          return sub
-        }
-
-        const navigated = butLast.reduce(
-          (ref, pathEntry) => ref[pathEntry],
-          cloned
-        )
-        navigated[last] = sub
-
-        return cloned
-      })
-    },
-    [setExpression]
-  )
-
-  return { expression, addExpression, removeExpression, updateFilter }
-}
 
 const Filter: React.FC<FilterProps> = ({
   fields,
@@ -162,9 +57,6 @@ const Filter: React.FC<FilterProps> = ({
     [fields]
   )
 
-  // TODO - between filter is invalid for dimensions so we need a way to filter
-  // which filters (ha) you can build.
-
   const fieldsFilter = useCallback(
     (dimension: GA4Dimension) => {
       return selectedFieldIds.has(dimension.apiName)
@@ -177,9 +69,6 @@ const Filter: React.FC<FilterProps> = ({
       setFilterExpression(expression)
     }
   }, [expression, setFilterExpression])
-
-  // TODO - look into use context to see if I can make these updateFilter,
-  // dimensionFilter, etc things a bit cleaner.
 
   return (
     <section className={classes.filter}>
---
+++ src/pages/ga4/query-explorer/_Filter/_useFilter.ts
@@ -0,0 +1,109 @@
+import { useState, useCallback } from "react"
+import { FilterExpression, UpdateFilter } from "./_index"
+import { ExpressionType } from "./_Expression"
+
+const useFilter = () => {
+  const [expression, setExpression] = useState<FilterExpression>({})
+
+  const updateFilter = useCallback<UpdateFilter>(
+    (path, update) => {
+      setExpression(old => {
+        const cloned = { ...old }
+        const butLast = [...path]
+        let last = butLast.pop()
+
+        const navigated = butLast.reduce(
+          (ref, pathEntry) => ref[pathEntry],
+          cloned
+        )
+
+        navigated[last as any] = update(navigated[last as any])
+
+        console.log({ last, butLast, navigated })
+
+        return cloned
+      })
+    },
+    [setExpression]
+  )
+
+  const removeExpression = useCallback(
+    (path: (string | number)[]) => {
+      setExpression(old => {
+        const cloned = { ...old }
+        const butLast = [...path]
+        let last = butLast.pop()
+
+        if (
+          last === "andGroup" ||
+          last === "orGroup" ||
+          last === "notExpression"
+        ) {
+          last = butLast.pop()
+        }
+
+        // If there is no last, that means we're at the top level
+        if (last === undefined) {
+          return {}
+        }
+
+        const navigated = butLast.reduce(
+          (ref, pathEntry) => ref[pathEntry],
+          cloned
+        )
+
+        if (Array.isArray(navigated)) {
+          navigated.splice(last as number, 1)
+        }
+
+        navigated[last] = {}
+
+        return cloned
+      })
+    },
+    [setExpression]
+  )
+
+  const addExpression = useCallback(
+    (path: (string | number)[], type: ExpressionType) => {
+      setExpression(old => {
+        const cloned = { ...old }
+        const butLast = [...path]
+        const last = butLast.pop()
+
+        const sub = subFor(type)
+
+        // If there is no last, that means we're at the top level
+        if (last === undefined) {
+          return sub
+        }
+
+        const navigated = butLast.reduce(
+          (ref, pathEntry) => ref[pathEntry],
+          cloned
+        )
+        navigated[last] = sub
+
+        return cloned
+      })
+    },
+    [setExpression]
+  )
+
+  return { expression, addExpression, removeExpression, updateFilter }
+}
+
+const subFor = (type: ExpressionType): FilterExpression => {
+  switch (type) {
+    case ExpressionType.And:
+      return { andGroup: { expressions: [] } }
+    case ExpressionType.Or:
+      return { orGroup: { expressions: [] } }
+    case ExpressionType.Not:
+      return { notExpression: {} }
+    case ExpressionType.Filter:
+      return { filter: {} }
+  }
+}
+
+export default useFilter
