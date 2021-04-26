18b766150dbc8470db013b3213232de18189806e
Matt Hamrick <matthewjhamrick@gmail.com>
2021-04-26 10:27:18 -0700

    continued refactor.

--- src/pages/campaign-url-builder/_GeneratedURL/_index.tsx
+++ src/pages/campaign-url-builder/_GeneratedURL/_index.tsx
@@ -1,9 +1,6 @@
 import * as React from "react"
 import useStyles from "../_useStyles"
-import { usePersistentBoolean } from "../../../hooks"
-import { StorageKey } from "../../../constants"
 import useShortenLink from "./_useShortenLink"
-import { websiteUrlFor } from "../_params"
 import Paper from "@material-ui/core/Paper"
 import { Error as ErrorIcon } from "@material-ui/icons"
 import WarningsFor from "./_WarningsFor"
@@ -13,6 +10,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Checkbox from "@material-ui/core/Checkbox"
 import CopyButton from "../../../components/CopyButton"
 import Button from "@material-ui/core/Button"
+import useWarningsFor from "./_useWarningsFor"
 
 interface GeneratedUrlProps {
   websiteUrl: string
@@ -33,108 +31,26 @@ const GeneratedURL: React.FC<GeneratedUrlProps> = ({
 }) => {
   const classes = useStyles()
 
-  const [generatedUrl, setGeneratedUrl] = React.useState("")
-  const [hasAllRequired, setHasAllRequired] = React.useState(false)
-  const [problematicUrl, setProblematicUrl] = React.useState(false)
-  const [longLink, setLongLink] = React.useState<string>(generatedUrl)
-  const [shortLink, setShortLink] = React.useState<undefined | string>()
-  const [showShort, setShowShort] = React.useState(false)
-  const [useFragment, setUseFragment] = usePersistentBoolean(
-    StorageKey.campaignBuilderUseFragment,
-    false
-  )
   const { authenticated, shorten, canShorten } = useShortenLink()
-
-  const shortenLinkGui = React.useCallback(() => {
-    if (showShort === true) {
-      // We're currently showing the short url and the user clicked "Show full
-      // URL". Set show short to false and return.
-      setShowShort(false)
-      return
-    }
-    // We can't shorten bit.ly links or links that are empty.
-    if (longLink === "" || longLink?.startsWith("https://bit.ly")) {
-      return
-    }
-    shorten(longLink).then(({ longLink, shortLink }) => {
-      setLongLink(longLink)
-      setShortLink(shortLink)
-      setShowShort(true)
-    })
-  }, [longLink, shorten, showShort])
-
-  React.useEffect(() => {
-    if (websiteUrl === "") {
-      setHasAllRequired(false)
-      return
-    }
-    if (source === "") {
-      setHasAllRequired(false)
-      return
-    }
-    if (medium === "") {
-      setHasAllRequired(false)
-      return
-    }
-    if (campaign === "") {
-      setHasAllRequired(false)
-      return
-    }
-    setHasAllRequired(true)
-  }, [websiteUrl, source, medium, campaign])
-
-  const setGeneratedFromInput = React.useCallback(() => {
-    if (!hasAllRequired) {
-      setGeneratedUrl("")
-      setLongLink("")
-      return
-    }
-    const generated = websiteUrlFor(
-      websiteUrl,
-      {
-        utm_source: source || undefined,
-        utm_medium: medium || undefined,
-        utm_campaign: campaign || undefined,
-        utm_term: term || undefined,
-        utm_content: content || undefined,
-      },
-      useFragment
-    )
-    setGeneratedUrl(generated)
-    setLongLink(generated)
-  }, [
+  const {
+    problematicUrl,
     hasAllRequired,
+    onWarning,
+    showShort,
+    shortLink,
+    longLink,
     useFragment,
+    setUseFragment,
+    shortenLinkGui,
+  } = useWarningsFor({
     websiteUrl,
     source,
     medium,
     campaign,
-    term,
+    shorten,
     content,
-  ])
-  // TODO - this can probably be fixed with useMemo based on what past me said:
-  // This is a bit of a hack, but I don't want to duplicate the code that sets
-  // the generated url.
-  React.useEffect(() => {
-    setGeneratedFromInput()
-    setShortLink(undefined)
-    setShowShort(false)
-  }, [
-    hasAllRequired,
-    setGeneratedFromInput,
-    useFragment,
-    websiteUrl,
-    source,
-    medium,
-    campaign,
     term,
-    content,
-  ])
-
-  const onWarning = React.useCallback(
-    warningPresent => setProblematicUrl(warningPresent),
-    [setProblematicUrl]
-  )
+  })
 
   return (
     <Paper className={classes.share}>
