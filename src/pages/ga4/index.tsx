234b4dc80bc8727b41475bc53885a2f7281750ab
Matt Hamrick <matthewjhamrick@gmail.com>
2021-07-21 15:37:49 -0700

    Fix three o typo (again) (#651)

--- src/pages/ga4/index.tsx
+++ src/pages/ga4/index.tsx
@@ -18,7 +18,7 @@ import Home from "@/components/Home"
 
 const IndexPage = ({ location: { pathname } }) => (
   <Layout
-    title="Discover the Gooogle Analytics platform"
+    title="Discover the Google Analytics platform"
     pathname={pathname}
     description="Google Analytics Demos & Tools is a resource for users and developers to discover what's possible with the Google Analytics Platform. Learn how to implement GA and applications that can be built to take advantage of the flexibility and power of Google Analytics."
   >
