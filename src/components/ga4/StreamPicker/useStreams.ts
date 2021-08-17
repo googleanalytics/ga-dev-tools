f3c7222c5b0a02f0483571ac1f176e2fbfde35e8
Matt Hamrick <matthewjhamrick@gmail.com>
2021-08-17 14:47:19 -0700

    added android and ios streams.

--- src/components/ga4/StreamPicker/useStreams.ts
+++ src/components/ga4/StreamPicker/useStreams.ts
@@ -2,15 +2,7 @@ import { useCallback, useEffect, useMemo } from "react"
 
 import useGapi from "@/hooks/useGapi"
 import { Requestable, RequestStatus } from "@/types"
-import {
-  // AndroidDataStream,
-  // IosDataStream,
-  PropertySummary,
-  Stream,
-  // Stream,
-  StreamType,
-  // WebDataStream,
-} from "@/types/ga4/StreamPicker"
+import { PropertySummary, Stream, StreamType } from "@/types/ga4/StreamPicker"
 import usePaginatedCallback from "@/hooks/usePaginatedCallback"
 import useCached from "@/hooks/useCached"
 import { StorageKey } from "@/constants"
@@ -18,9 +10,19 @@ import moment from "moment"
 import useRequestStatus from "@/hooks/useRequestStatus"
 
 type WebStreamsResponse = gapi.client.analyticsadmin.GoogleAnalyticsAdminV1alphaListWebDataStreamsResponse
+type IOSStreamsResponse = gapi.client.analyticsadmin.GoogleAnalyticsAdminV1alphaListIosAppDataStreamsResponse
+type AndroidStreamsResponse = gapi.client.analyticsadmin.GoogleAnalyticsAdminV1alphaListAndroidAppDataStreamsResponse
+
 const getWebStreams = (response: WebStreamsResponse) => response.webDataStreams
+const getIOSStreams = (response: IOSStreamsResponse) =>
+  response.iosAppDataStreams
+const getAndroidStreams = (response: AndroidStreamsResponse) =>
+  response.androidAppDataStreams
 
 const getWebPageToken = (response: WebStreamsResponse) => response.nextPageToken
+const getIOSPageToken = (response: IOSStreamsResponse) => response.nextPageToken
+const getAndroidPageToken = (response: IOSStreamsResponse) =>
+  response.nextPageToken
 
 const useStreams = (
   property: PropertySummary | undefined,
@@ -37,14 +39,11 @@ const useStreams = (
     setFailed: setWebStreamFailed,
   } = useRequestStatus()
 
-  useEffect(() => {
-    setWebStreamNotStarted()
-  }, [property, setWebStreamNotStarted])
-
   const requestReady = useMemo(
     () => adminAPI !== undefined && property !== undefined,
     [adminAPI, property]
   )
+
   const paginatedWebStreamsRequest = useCallback(
     (pageToken: string | undefined) => {
       if (adminAPI === undefined || property === undefined) {
@@ -59,24 +58,7 @@ const useStreams = (
     },
     [adminAPI, property]
   )
-  // useEffect(() => {
-  //   console.log(
-  //     "paginatedWebStreamsRequest changed",
-  //     paginatedWebStreamsRequest
-  //   )
-  // }, [paginatedWebStreamsRequest])
-  //
-  // What I'm observing.
-  //
-  // When I change the property, I'm no longer able to choose a stream, because
-  // the list of streams doesn't match with the property.
-  //
-  // The stream dropdown values are correct.
-  //
-  // The stream values that are wrong are the ones that are used in the getById
-  // for the useHydrated call.
-  //
-  // This is the call happens in the useAccountPropertyStreams.
+
   const requestWebStreams = usePaginatedCallback(
     requestReady,
     "Invalid invariant - property & adminAPI must be defined.",
@@ -87,13 +69,14 @@ const useStreams = (
     setWebStreamFailed
   )
 
-  const storageKey = useMemo(
-    () => `${StorageKey.ga4WebStreams}/${property?.property}` as StorageKey,
+  const webStorageKey = useMemo(
+    () =>
+      `${StorageKey.ga4WebStreams}/${property?.property}/webStreams` as StorageKey,
     [property?.property]
   )
 
   const webStreams = useCached(
-    storageKey,
+    webStorageKey,
     requestWebStreams,
     moment.duration(5, "minutes"),
     requestReady
@@ -102,206 +85,188 @@ const useStreams = (
   useEffect(() => {
     if (webStreams !== undefined) {
       setWebStreamSuccessful()
+    }
+  }, [setWebStreamSuccessful, webStreams])
+
+  const {
+    status: iosStreamsStatus,
+    setInProgress: setIOSStreamInProgress,
+    setSuccessful: setIOSStreamSuccessful,
+    setNotStarted: setIOSStreamNotStarted,
+    setFailed: setIOSStreamFailed,
+  } = useRequestStatus()
+
+  const paginatedIOSStreamsRequest = useCallback(
+    (pageToken: string | undefined) => {
+      if (adminAPI === undefined || property === undefined) {
+        throw new Error(
+          "Invalid invariant - property and adminAPI must be defined."
+        )
+      }
+      return adminAPI.properties.iosAppDataStreams.list({
+        parent: property?.property!,
+        pageToken,
+      })
+    },
+    [adminAPI, property]
+  )
+
+  const requestIOSStreams = usePaginatedCallback(
+    requestReady,
+    "Invalid invariant - property & adminAPI must be defined.",
+    paginatedIOSStreamsRequest,
+    getIOSStreams,
+    getIOSPageToken,
+    setIOSStreamInProgress,
+    setIOSStreamFailed
+  )
+
+  const iosStorageKey = useMemo(
+    () =>
+      `${StorageKey.ga4WebStreams}/${property?.property}/iosStreams` as StorageKey,
+    [property?.property]
+  )
+
+  const iosStreams = useCached(
+    iosStorageKey,
+    requestIOSStreams,
+    moment.duration(5, "minutes"),
+    requestReady
+  )
+
+  useEffect(() => {
+    if (iosStreams !== undefined) {
+      setIOSStreamSuccessful()
+    }
+  }, [setIOSStreamSuccessful, iosStreams])
+
+  const {
+    status: androidStreamsStatus,
+    setInProgress: setAndroidStreamInProgress,
+    setSuccessful: setAndroidStreamSuccessful,
+    setNotStarted: setAndroidStreamNotStarted,
+    setFailed: setAndroidStreamFailed,
+  } = useRequestStatus()
+
+  const paginatedAndroidStreamsRequest = useCallback(
+    (pageToken: string | undefined) => {
+      if (adminAPI === undefined || property === undefined) {
+        throw new Error(
+          "Invalid invariant - property and adminAPI must be defined."
+        )
+      }
+      return adminAPI.properties.androidAppDataStreams.list({
+        parent: property?.property!,
+        pageToken,
+      })
+    },
+    [adminAPI, property]
+  )
+
+  const requestAndroidStreams = usePaginatedCallback(
+    requestReady,
+    "Invalid invariant - property & adminAPI must be defined.",
+    paginatedAndroidStreamsRequest,
+    getAndroidStreams,
+    getAndroidPageToken,
+    setAndroidStreamInProgress,
+    setAndroidStreamFailed
+  )
+
+  const androidStorageKey = useMemo(
+    () =>
+      `${StorageKey.ga4WebStreams}/${property?.property}/androidStreams` as StorageKey,
+    [property?.property]
+  )
+
+  const androidStreams = useCached(
+    androidStorageKey,
+    requestAndroidStreams,
+    moment.duration(5, "minutes"),
+    requestReady
+  )
+
+  useEffect(() => {
+    if (androidStreams !== undefined) {
+      setAndroidStreamSuccessful()
+    }
+  }, [setAndroidStreamSuccessful, androidStreams])
+
+  useEffect(() => {
+    if (
+      webStreams !== undefined &&
+      iosStreams !== undefined &&
+      androidStreams !== undefined
+    ) {
       onComplete && onComplete()
     }
-  }, [setWebStreamSuccessful, webStreams, onComplete])
+  }, [onComplete, webStreams, iosStreams, androidStreams])
 
-  if (webStreamsStatus === RequestStatus.Successful) {
+  useEffect(() => {
+    setWebStreamNotStarted()
+    setIOSStreamNotStarted()
+    setAndroidStreamNotStarted()
+  }, [
+    property,
+    setWebStreamNotStarted,
+    setIOSStreamNotStarted,
+    setAndroidStreamNotStarted,
+  ])
+
+  if (
+    webStreamsStatus === RequestStatus.Successful &&
+    iosStreamsStatus === RequestStatus.Successful &&
+    androidStreamsStatus === RequestStatus.Successful
+  ) {
     if (webStreams === undefined) {
       throw new Error("Invalid invariant - webStreams must be defined here.")
     }
+    if (iosStreams === undefined) {
+      throw new Error("Invalid invariant - iosStreams must be defined here.")
+    }
+    if (androidStreams === undefined) {
+      throw new Error(
+        "Invalid invariant - androidStreams must be defined here."
+      )
+    }
     return {
       status: RequestStatus.Successful,
-      streams: webStreams.map(s => ({
-        type: StreamType.WebDataStream,
-        value: s,
-      })),
+      streams: webStreams
+        .map(s => ({
+          type: StreamType.WebDataStream,
+          value: s,
+        }))
+        .concat(
+          iosStreams.map(s => ({
+            type: StreamType.IOSDataStream,
+            value: s,
+          }))
+        )
+        .concat(
+          androidStreams.map(s => ({
+            type: StreamType.AndroidDataStream,
+            value: s,
+          }))
+        ),
     }
   }
 
-  if (webStreamsStatus === RequestStatus.InProgress) {
+  if (
+    webStreamsStatus === RequestStatus.InProgress ||
+    iosStreamsStatus === RequestStatus.InProgress ||
+    androidStreamsStatus === RequestStatus.InProgress
+  ) {
     return { status: RequestStatus.InProgress }
   }
 
-  if (webStreamsStatus === RequestStatus.Failed) {
+  if (
+    webStreamsStatus === RequestStatus.Failed ||
+    iosStreamsStatus === RequestStatus.Failed ||
+    androidStreamsStatus === RequestStatus.Failed
+  ) {
     return { status: RequestStatus.Failed }
   }
   return { status: RequestStatus.NotStarted }
-
-  //const requestWebStreams = useCallback(() => {
-  //  try {
-  //    if (adminAPI === undefined || property === undefined) {
-  //      throw new Error(
-  //        "Invalid invariant - adminAPI and property must be defined."
-  //      )
-  //    }
-  //    setWebStreamsStatus(RequestStatus.InProgress)
-  //    let pageToken: string | undefined = undefined
-  //    let streams: WebDataStream[] = []
-  //    do {
-  //      pageToken = undefined
-  //      const {result} = await adminAPI.properties.webDataStreams
-  //      .list({
-  //        parent: property.property!,
-  //        pageToken: webPageToken,
-  //      })
-
-  //      streams = streams.concat(
-  //        result.webDataStreams || []
-  //      )
-
-  //      if (result.nextPageToken) {
-  //        pageToken = result.nextPageToken
-  //      }
-  //    } while (pageToken !== undefined)
-  //    setWebStreamsStatus(RequestStatus.Successful)
-  //    return streams
-  //  } catch (e) {
-  //}, [])
-
-  //const requestAndroidStreams = useCallback(() => {
-  //  //
-  //}, [])
-
-  //const requestIOSStreams = useCallback(() => {
-  //  //
-  //}, [])
-
-  //const [webStatus, setWebStatus] = useState(RequestStatus.NotStarted)
-  //const [web, setWeb] = useState<WebDataStream[]>()
-  //const [webPageToken, setWebPageToken] = useState<string>()
-
-  //const [androidStatus, setAndroidStatus] = useState(RequestStatus.NotStarted)
-  //const [android, setAndroid] = useState<AndroidDataStream[]>()
-  //const [androidPageToken, setAndroidPageToken] = useState<string>()
-
-  //const [iosStatus, setIosStatus] = useState(RequestStatus.NotStarted)
-  //const [ios, setIos] = useState<IosDataStream[]>()
-  //const [iosPageToken, setIosPageToken] = useState<string>()
-
-  //const streams: Stream[] = useMemo(() => {
-  //  return (web || []).concat(android || []).concat(ios || [])
-  //}, [web, android, ios])
-
-  //useEffect(() => {
-  //  if (property === undefined) {
-  //    return
-  //  }
-  //  if (webStatus === RequestStatus.NotStarted) {
-  //    setWebStatus(RequestStatus.InProgress)
-  //  }
-  //  if (webStatus === RequestStatus.InProgress && gapi !== undefined) {
-  //    gapi.client.analyticsadmin.properties.webDataStreams
-  //      .list({
-  //        parent: property,
-  //        pageToken: webPageToken,
-  //      })
-  //      .then(response => {
-  //        const nextToken = response.result.nextPageToken
-  //        setWeb((old = []) => old.concat(response.result.webDataStreams || []))
-  //        if (nextToken === undefined) {
-  //          setWebStatus(RequestStatus.Successful)
-  //        } else {
-  //          setWebPageToken(nextToken)
-  //        }
-  //      })
-  //      .catch(e => {
-  //        console.error({ e })
-  //        setWebStatus(RequestStatus.Failed)
-  //      })
-  //  }
-  //}, [gapi, webPageToken, webStatus, property])
-
-  //useEffect(() => {
-  //  if (property === undefined) {
-  //    return
-  //  }
-  //  if (androidStatus === RequestStatus.NotStarted) {
-  //    setAndroidStatus(RequestStatus.InProgress)
-  //  }
-  //  if (androidStatus === RequestStatus.InProgress && gapi !== undefined) {
-  //    gapi.client.analyticsadmin.properties.androidAppDataStreams
-  //      .list({
-  //        parent: property,
-  //        pageToken: androidPageToken,
-  //      })
-  //      .then(response => {
-  //        const nextToken = response.result.nextPageToken
-  //        setAndroid((old = []) =>
-  //          old.concat(response.result.androidAppDataStreams || [])
-  //        )
-  //        if (nextToken === undefined) {
-  //          setAndroidStatus(RequestStatus.Successful)
-  //        } else {
-  //          setAndroidPageToken(nextToken)
-  //        }
-  //      })
-  //      .catch(e => {
-  //        console.error({ e })
-  //        setWebStatus(RequestStatus.Failed)
-  //      })
-  //  }
-  //}, [gapi, androidPageToken, androidStatus, property])
-
-  //useEffect(() => {
-  //  if (property === undefined) {
-  //    return
-  //  }
-  //  if (iosStatus === RequestStatus.NotStarted) {
-  //    setIosStatus(RequestStatus.InProgress)
-  //  }
-  //  if (iosStatus === RequestStatus.InProgress && gapi !== undefined) {
-  //    gapi.client.analyticsadmin.properties.iosAppDataStreams
-  //      .list({
-  //        parent: property,
-  //        pageToken: iosPageToken,
-  //      })
-  //      .then(response => {
-  //        const nextToken = response.result.nextPageToken
-  //        setIos((old = []) =>
-  //          old.concat(response.result.iosAppDataStreams || [])
-  //        )
-  //        if (nextToken === undefined) {
-  //          setIosStatus(RequestStatus.Successful)
-  //        } else {
-  //          setIosPageToken(nextToken)
-  //        }
-  //      })
-  //      .catch(e => {
-  //        console.error({ e })
-  //        setWebStatus(RequestStatus.Failed)
-  //      })
-  //  }
-  //}, [gapi, iosPageToken, iosStatus, property])
-
-  // if (
-  //   webStatus === RequestStatus.Successful &&
-  //   androidStatus === RequestStatus.Successful &&
-  //   iosStatus === RequestStatus.Successful
-  // ) {
-  //   return {
-  //     status: webStatus,
-  //     web: web || [],
-  //     android: android || [],
-  //     ios: ios || [],
-  //     streams,
-  //   }
-  // }
-  // if (
-  //   webStatus === RequestStatus.Failed ||
-  //   androidStatus === RequestStatus.Failed ||
-  //   iosStatus === RequestStatus.Failed
-  // ) {
-  //   return { status: RequestStatus.Failed }
-  // }
-  // if (
-  //   webStatus === RequestStatus.NotStarted &&
-  //   androidStatus === RequestStatus.NotStarted &&
-  //   iosStatus === RequestStatus.NotStarted
-  // ) {
-  //   return { status: RequestStatus.NotStarted }
-  // }
-  // return { status: RequestStatus.InProgress }
 }
 
 export default useStreams
