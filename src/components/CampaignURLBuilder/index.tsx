ebf248b5f889771b6cde61eb804cf845e68bb189
ikuleshov <ikuleshov@google.com>
2022-07-12 10:03:00 +0200

    Remove iOS Campaign URL Builder for GA4 (#937)

--- src/components/CampaignURLBuilder/index.tsx
+++ src/components/CampaignURLBuilder/index.tsx
@@ -45,8 +45,8 @@ export const CampaignURLBuilder: React.FC<CampaignURLBuilderProps> = ({
         return 0
       case URLBuilderType.Play:
         return 1
-      case URLBuilderType.Ios:
-        return 2
+      default:
+        return 0
     }
   }, [type])
 
@@ -56,8 +56,6 @@ export const CampaignURLBuilder: React.FC<CampaignURLBuilderProps> = ({
         return `/campaign-url-builder/`
       case 1:
         return `/campaign-url-builder/play/`
-      case 2:
-        return `/campaign-url-builder/ios/`
       default:
         throw new Error("No matching idx")
     }
@@ -76,7 +74,6 @@ export const CampaignURLBuilder: React.FC<CampaignURLBuilderProps> = ({
       >
         <Tab label="Web" />
         <Tab label="Play" />
-        <Tab label="iOS" />
       </Tabs>
 
       <TabPanel value={tab} index={0}>
@@ -84,9 +81,6 @@ export const CampaignURLBuilder: React.FC<CampaignURLBuilderProps> = ({
       </TabPanel>
       <TabPanel value={tab} index={1}>
         <PlayURLBuilder version={version} />
-      </TabPanel>
-      <TabPanel value={tab} index={2}>
-        <IOSURLBuilder version={version} />
       </TabPanel>
     </>
   )
