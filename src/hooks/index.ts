5b2022b1a2a9231857025c2eb1ec9c7c97a578a9
ikuleshov <ikuleshov@google.com>
2023-12-14 12:41:43 +0000

    Set GA4 version by default

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -197,13 +197,13 @@ const getRedirectInfo = (
           }
         case "/ga4/":
           return {
-            redirectPath: "/",
-            toast: "Redirecting to the UA home page.",
+            redirectPath: "/account-explorer/",
+            toast: "Redirecting to the UA Account Explorer.",
           }
         default:
           return {
-            redirectPath: "/",
-            toast: "No UA demo. Redirecting to the UA home page.",
+            redirectPath: "/account-explorer/",
+            toast: "Redirecting to the UA Account Explorer page.",
           }
       }
     }
